fcafaf894cf8ff03a1803b6197bdecd572a387d9
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-04-30 04:38:48 +0900

    chore: add constant file

---
+++ Adrenalist/Adrenalist/Helper/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  Adrenalist
+//
+//  Created by 김윤석 on 2022/04/30.
+//
+
+import Foundation
+
+enum Constant {
+    
+    enum Button {
+        static let editting = ""
+    }
+}
