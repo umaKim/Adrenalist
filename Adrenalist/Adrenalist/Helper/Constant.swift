ce912d6705c33286170504b277aa680e5b8841af
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-05-02 03:48:28 +0900

    chore: add Constants

--- Adrenalist/Adrenalist/Helper/Constant.swift
+++ Adrenalist/Adrenalist/Helper/Constant.swift
@@ -10,6 +10,9 @@ import Foundation
 enum Constant {
     
     enum Button {
-        static let editting = ""
+        static let editting     = "slider.horizontal.3"
+        static let addWorkout   = "plus.square"
+        static let calendar     = "calendar.circle"
+        static let setting      = "gearshape"
     }
 }
