56e41175cbbd801b6fbe9c5dc7579899eef31816
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-05-02 03:51:06 +0900

    feat: add cell for suggetion Collection view

---
+++ Adrenalist/Adrenalist/Core/Main/View/WorkoutList/View/SuggestionListCell.swift
@@ -0,0 +1,42 @@
+//
+//  SuggestionListCell.swift
+//  Adrenalist
+//
+//  Created by 김윤석 on 2022/04/30.
+//
+
+import UIKit.UICollectionViewCell
+
+final class SuggestionListCell: UICollectionViewCell {
+    static let identifier = "SuggestionListCell"
+    
+    private let titleLabel: UILabel = {
+       let lb = UILabel()
+        lb.translatesAutoresizingMaskIntoConstraints = false
+        return lb
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    func configure(with workout: Workout) {
+        titleLabel.text = workout.title
+        backgroundColor = .blue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
