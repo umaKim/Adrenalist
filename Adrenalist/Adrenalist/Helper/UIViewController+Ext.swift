2b7a9cafc48158b1abc78d16544b5545632b07f8
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-05-04 01:50:16 +0900

    feat: add keyboard dismiss functionality

---
+++ Adrenalist/Adrenalist/Helper/UIViewController+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewController+Ext.swift
+//  Adrenalist
+//
+//  Created by 김윤석 on 2022/05/04.
+//
+
+import UIKit.UIView
+
+extension UIView {
+    func addTapGestureToViewForKeyboardDismiss() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIView.endEditing))
+        addGestureRecognizer(tap)
+    }
+}
+
