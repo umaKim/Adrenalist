42c66be375280316bededfcc6cbe29c2f9206134
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-05-15 21:52:27 +0900

    chore: rename

--- Adrenalist/Adrenalist/Core/WorkoutHistory/View/WorkoutHistoryHeaderView.swift
+++ Adrenalist/Adrenalist/Core/WorkoutHistory/View/WorkoutHistoryHeaderView.swift
@@ -1,26 +1,33 @@
 //
-//  WorkoutHistoryHeaderTableViewCell.swift
+//  WorkoutHistoryHeaderView.swift
 //  Adrenalist
 //
 //  Created by 김윤석 on 2022/04/27.
 //
 
+import Combine
 import FSCalendar
 import UIKit
 
-final class WorkoutHistoryHeaderTableViewCell: UITableViewHeaderFooterView {
+enum WorkoutHistoryHeaderViewAction {
+    case didTapDate
+}
+
+final class WorkoutHistoryHeaderView: UITableViewHeaderFooterView {
     static let identifier = "WorkoutHistoryHeaderTableViewCell"
+    
+    private(set) lazy var actionPublisher = actionSubject.eraseToAnyPublisher()
+    private let actionSubject = PassthroughSubject<WorkoutHistoryHeaderViewAction, Never>()
     
     private let fsCalendar = FSCalendar()
     
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
+        
+        fsCalendar.appearance.titleDefaultColor = .white
+        
         setupHeaderView()
         configureLayout()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
     }
     
     private func setupHeaderView() {
@@ -34,5 +41,21 @@ final class WorkoutHistoryHeaderTableViewCell: UITableViewHeaderFooterView {
             fsCalendar.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             fsCalendar.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 3)
         ])
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+extension WorkoutHistoryHeaderView: FSCalendarDataSource {
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        3
+    }
+}
+
+extension WorkoutHistoryHeaderView: FSCalendarDelegate {
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        
     }
 }
