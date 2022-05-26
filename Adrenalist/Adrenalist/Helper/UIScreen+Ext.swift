3e1286749610d7edb7918eda5514f45810360104
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-05-26 20:13:51 +0900

    chore: add UIScreen ext for width and height

---
+++ Adrenalist/Adrenalist/Helper/UIScreen+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  UIScreen+Ext.swift
+//  Adrenalist
+//
+//  Created by 김윤석 on 2022/05/26.
+//
+
+import UIKit
+
+extension UIScreen {
+    var width: CGFloat {
+        bounds.width
+    }
+    
+    var height: CGFloat {
+        bounds.height
+    }
+}
