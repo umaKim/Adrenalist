97cd9013df980ae4120a97f40079ba7b5562dcca
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-05-26 20:21:36 +0900

    feat: add padding values for textfield

--- Adrenalist/Adrenalist/GlobalViews/AdrenalistTextField.swift
+++ Adrenalist/Adrenalist/GlobalViews/AdrenalistTextField.swift
@@ -8,13 +8,27 @@
 import UIKit.UITextField
 
 final class AdrenalistTextField: UITextField {
+    private let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    
     init(placeHolder: String) {
         super.init(frame: .zero)
         
         self.placeholder = placeHolder
         layer.borderWidth = 1
-//        layer.borderColor = UIColor.blue.cgColor
         layer.borderColor = UIColor.pinkishRed.cgColor
+        layer.cornerRadius = 12
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
     }
     
     required init?(coder: NSCoder) {
