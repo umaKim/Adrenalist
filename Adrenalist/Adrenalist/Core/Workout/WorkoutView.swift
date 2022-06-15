b37eb2a0be9aa936c709bc3599bf5a1aab19c562
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-15 21:03:35 +0900

    feat: add favorite and workoutlist collectionviews and add buttons

--- Adrenalist/Adrenalist/Core/Workout/WorkoutView.swift
+++ Adrenalist/Adrenalist/Core/Workout/WorkoutView.swift
@@ -14,6 +14,10 @@ enum WorkoutListViewAction {
     case didTapCalendar
     case didTapSetting
     case didTap(MyScrollableDatepickerModel)
+    
+    case didTapAdd
+    case didTapEdit
+    case titleCalendarDidTap(String)
 }
 
 final class WorkoutListView: UIView {
@@ -21,10 +25,32 @@ final class WorkoutListView: UIView {
     private(set) lazy var actionPublisher = actionSubject.eraseToAnyPublisher()
     private let actionSubject = PassthroughSubject<WorkoutListViewAction, Never>()
     
-    private(set) lazy var calendarButton = UIBarButtonItem(image: UIImage(systemName: Constant.ButtonImage.calendar), style: .done, target: nil, action: nil)
-    private(set) lazy var settingButton = UIBarButtonItem(image: UIImage(systemName: Constant.ButtonImage.setting), style: .done, target: nil, action: nil)
+    private(set) lazy var editButton = UIBarButtonItem(title: "Edit", image: nil, primaryAction: nil, menu: UIMenu(options: .displayInline, children: menuItems))
     
-    lazy var calendarView: MyScrollableDatepicker = {
+    private var menuItems: [UIAction] {
+        return [
+            UIAction(title: "순서 변경", image: UIImage(systemName: "sun.max"), handler: { (_) in
+            }),
+            UIAction(title: "내일로 미루기", image: UIImage(systemName: "moon"), handler: { (_) in
+            }),
+            UIAction(title: "삭제", image: UIImage(systemName: "trash"), attributes: .destructive, handler: { (_) in
+            })
+        ]
+    }
+    
+    private(set) lazy var addButton: UIBarButtonItem = {
+        let lb = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: nil, action: nil)
+        lb.tintColor = .white
+        return lb
+    }()
+    
+    private(set) lazy var calendarTitleButton: UIButton = {
+        let bt = UIButton()
+        bt.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        return bt
+    }()
+    
+    public lazy var calendarView: MyScrollableDatepicker = {
         let cv = MyScrollableDatepicker()
         
         cv.selectedDate = Date()
@@ -46,32 +72,40 @@ final class WorkoutListView: UIView {
         return cv
     }()
     
-    private lazy var favoritesCollectionView: UICollectionView = {
+    private lazy var dividerView: UIView = {
+        let uv = UIView()
+        uv.backgroundColor = .lightGray
+        uv.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+        return uv
+    }()
+    
+    private(set) lazy var favoritesCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 12
         layout.scrollDirection = .horizontal
         layout.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-//        cv.dataSource = self
-//        cv.delegate = self
-        cv.register(MyScrollableDatepickerCell.self, forCellWithReuseIdentifier: MyScrollableDatepickerCell.identifier)
+        //        cv.dataSource = self
+        //        cv.delegate = self
+        cv.register(FavoriteCollectionViewCell.self, forCellWithReuseIdentifier: FavoriteCollectionViewCell.identifier)
         cv.showsHorizontalScrollIndicator = false
         cv.translatesAutoresizingMaskIntoConstraints = false
         cv.backgroundColor = .black
+        cv.heightAnchor.constraint(equalToConstant: 70).isActive = true
         return cv
     }()
     
-    private lazy var workoutlistCollectionView: UICollectionView = {
+    private(set) lazy var workoutlistCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 12
-        layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 0, right: 16)
         
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-//        cv.dataSource = self
-//        cv.delegate = self
-        cv.register(MyScrollableDatepickerCell.self, forCellWithReuseIdentifier: MyScrollableDatepickerCell.identifier)
+        //        cv.dataSource = self
+        //        cv.delegate = self
+        cv.register(WorkoutlistCollectionViewCell.self, forCellWithReuseIdentifier: WorkoutlistCollectionViewCell.identifier)
         cv.showsHorizontalScrollIndicator = false
         cv.translatesAutoresizingMaskIntoConstraints = false
         cv.backgroundColor = .black
@@ -116,7 +150,7 @@ final class WorkoutListView: UIView {
             if let reps = updateWorkout.reps {
                 self.repsLabel.text = "\(reps) Reps"
             }
-        
+            
             if let timer = updateWorkout.timer {
                 self.repsLabel.text = "\(timer) sec"
             }
@@ -163,25 +197,47 @@ final class WorkoutListView: UIView {
     
     //MARK: - Private Methods
     private func bind() {
-        calendarButton
-            .tapPublisher
-            .sink {[weak self] _ in
-                guard let self = self else {return }
-                self.actionSubject.send(.didTapCalendar)
-            }
-            .store(in: &cancellables)
+        //        calendarButton
+        //            .tapPublisher
+        //            .sink {[weak self] _ in
+        //                guard let self = self else {return }
+        //                self.actionSubject.send(.didTapCalendar)
+        //            }
+        //            .store(in: &cancellables)
         
-        settingButton
-            .tapPublisher
-            .sink {[weak self] _ in
-                guard let self = self else {return }
-                self.actionSubject.send(.didTapSetting)
-            }
-            .store(in: &cancellables)
+        //        settingButton
+        //            .tapPublisher
+        //            .sink {[weak self] _ in
+        //                guard let self = self else {return }
+        //                self.actionSubject.send(.didTapSetting)
+        //            }
+        //            .store(in: &cancellables)
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
         tapGesture.numberOfTapsRequired = 2
         addGestureRecognizer(tapGesture)
+        
+//        editButton
+//            .tapPublisher
+//            .sink { [weak self] _ in
+//                self?.actionSubject.send(.didTapAdd)
+//            }
+//            .store(in: &cancellables)
+        
+        addButton
+            .tapPublisher
+            .sink { [weak self] _ in
+                self?.actionSubject.send(.didTapEdit)
+            }
+            .store(in: &cancellables)
+        
+        calendarTitleButton
+            .tapPublisher
+            .sink { _ in
+                print("tapped cAlendar")
+                //                self.actionSubject.send(.titleCalendarDidTap(<#T##String#>))
+            }
+            .store(in: &cancellables)
     }
     
     @objc
@@ -192,34 +248,29 @@ final class WorkoutListView: UIView {
     private func setupUI() {
         backgroundColor = .black
         
-        let labelStackView = UIStackView(arrangedSubviews: [repsLabel, weightLabel])
-        labelStackView.axis = .vertical
-        labelStackView.distribution = .fill
-        labelStackView.alignment = .center
-        labelStackView.spacing = 12
+        let cvStackView = UIStackView(arrangedSubviews: [favoritesCollectionView, dividerView, workoutlistCollectionView])
+        cvStackView.axis = .vertical
+        cvStackView.distribution = .fill
+        cvStackView.alignment = .fill
+        cvStackView.spacing = 2
         
-        [calendarView,
-//         circularView,
-         workoutLabel, nextLabel, labelStackView].forEach {
+        [calendarView, cvStackView].forEach {
             addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
         
         NSLayoutConstraint.activate([
+            
             calendarView.centerXAnchor.constraint(equalTo: centerXAnchor),
             calendarView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             calendarView.leadingAnchor.constraint(equalTo: leadingAnchor),
             calendarView.trailingAnchor.constraint(equalTo: trailingAnchor),
             calendarView.heightAnchor.constraint(equalToConstant: 80),
             
-            workoutLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            workoutLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
-            
-            nextLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            nextLabel.topAnchor.constraint(equalTo: workoutLabel.bottomAnchor, constant: frame.height / 13),
-            
-            labelStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            labelStackView.topAnchor.constraint(equalTo: centerYAnchor, constant: frame.height / 3.5)
+            cvStackView.topAnchor.constraint(equalTo: calendarView.bottomAnchor),
+            cvStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            cvStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            cvStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
     
@@ -229,16 +280,25 @@ final class WorkoutListView: UIView {
 }
 
 extension WorkoutListView: MyScrollableDatepickerDelegate {
-    func datepicker(_ datepicker: MyScrollableDatepicker, didSelectDate date: MyScrollableDatepickerModel) {
-//        let date = MyScrollableDatepickerModel(date: date.date, isSelected: true)
-//        self.actionSubject.send(.didTap(date))
-//        let index = datepicker.dates.firstIndex(of: date)?.description ?? "0"
-//        let indexInt = Int(index) ?? 0
-//
-//        datepicker.dates[indexInt] = date
-//        let indexPath = IndexPath(row: indexInt, section: 0)
-//        datepicker.collectionView.reloadItems(at: [indexPath])
-        datepicker.updateDateSet(with: date)
-        
+    func datepicker(
+        _ datepicker: MyScrollableDatepicker,
+        didScroll index: IndexPath
+    ) {
+        calendarTitleButton.setTitle("\( datepicker.dates[index.row].date.getFormattedDate(format: "yyyy년 MM월"))", for: .normal)
     }
+    
+    func datepicker(
+        _ datepicker: MyScrollableDatepicker,
+        didSelectDate date: MyScrollableDatepickerModel
+    ) {
+        datepicker.updateDateSet(with: date)
+    }
+}
+
+struct WorkoutModel {
+    let title: String
+    let reps: Int?
+    let weight: Double?
+    let timer: TimeInterval?
+    let isFavorite: Bool?
 }
