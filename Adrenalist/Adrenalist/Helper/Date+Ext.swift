3c6468127bb68ae1dafd1a7eeb67632bea046a29
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-16 19:33:17 +0900

    chore: add extension for Date

--- Adrenalist/Adrenalist/Helper/Date+Ext.swift
+++ Adrenalist/Adrenalist/Helper/Date+Ext.swift
@@ -14,3 +14,11 @@ extension Date {
         return date!
     }
 }
+
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
+}
