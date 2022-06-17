90a04fb56032054701e11be2741fb4beffa58762
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-17 22:05:57 +0900

    chore: set different file for workoutmodel

---
+++ Adrenalist/Adrenalist/Model/WorkoutModel.swift
@@ -0,0 +1,16 @@
+//
+//  WorkoutModel.swift
+//  Adrenalist
+//
+//  Created by 김윤석 on 2022/06/17.
+//
+
+import Foundation
+
+struct WorkoutModel {
+    let title: String
+    let reps: Int?
+    let weight: Double?
+    let timer: TimeInterval?
+    let isFavorite: Bool?
+}
