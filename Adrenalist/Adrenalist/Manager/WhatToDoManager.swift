9fb07836522f5bde3010d6cc1e9a68ffac003d33
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-23 21:06:00 +0900

    chore: set new Persistance

--- Adrenalist/Adrenalist/Manager/WhatToDoManager.swift
+++ Adrenalist/Adrenalist/Manager/WhatToDoManager.swift
@@ -15,41 +15,46 @@ enum DataType: String {
 
 final class ItemManager {
     static let shared = ItemManager()
-    
-    @Published private(set) var suggestions: [Item] = []
-    @Published private(set) var itemToDos: [Item] = []
-    
+
+    @Published private(set) var suggestions: [WorkoutModel] = []
+    @Published private(set) var itemToDos: [WorkoutModel] = []
+
     //MARK: - Public
-    public func appendWorkoutToDos(_ workout: Item) {
+    public func appendWorkoutToDos(_ workout: WorkoutModel) {
         self.itemToDos.append(workout)
         self.save(itemToDos, for: .workouts)
     }
-    
-    public func updateWorkoutToDos(_ workouts: [Item]) {
+
+    public func updateWorkoutToDos(_ workouts: [WorkoutModel]) {
         self.itemToDos = workouts
         self.save(workouts, for: .workouts)
     }
-    
-    public func appendSuggetion(_ suggestion: Item) {
+
+    public func appendSuggetion(_ suggestion: WorkoutModel) {
         self.suggestions.append(suggestion)
         self.save(suggestions, for: .suggestions)
     }
-    
-    public func updateSuggestions(_ suggestions: [Item]) {
+
+    public func updateSuggestions(_ suggestions: [WorkoutModel]) {
         self.suggestions = suggestions
         self.save(suggestions, for: .suggestions)
     }
-    
+
+    public func fetchWorkoutlist(at date: Date) {
+//        self.fetchWorkoutlist(at: date)
+        self.retrieve(.workouts)
+    }
+
     //MARK: - Privates
-    
+
     private let defaults = UserDefaults.standard
     private enum Keys {
         static let workouts = "workouts"
         static let suggestions = "suggestions"
         static let history = "history"
     }
-    
-    private func save(_ workouts: [Item], for type: DataType) {
+
+    private func save(_ workouts: [WorkoutModel], for type: DataType) {
         do {
             let encoder = JSONEncoder()
             let encodedFavorites = try encoder.encode(workouts)
@@ -58,29 +63,28 @@ final class ItemManager {
             print(error)
         }
     }
-    
-    private func retrieve(_ type: DataType) -> AnyPublisher<[Item], Error> {
+
+    private func retrieve(_ type: DataType) -> AnyPublisher<[WorkoutModel], Error> {
         guard let data = defaults.object(forKey: type.rawValue) as? Data else {
             return Just([]).setFailureType(to: Error.self).eraseToAnyPublisher()
         }
-        
+
         do {
             let decoder = JSONDecoder()
-            let workouts = try decoder.decode([Item].self, from: data)
+            let workouts = try decoder.decode([WorkoutModel].self, from: data)
             return Just(workouts).setFailureType(to: Error.self).eraseToAnyPublisher()
         } catch {
             return Fail(error: error).eraseToAnyPublisher()
         }
     }
-    
+
     private var cancellables: Set<AnyCancellable>
-    
+
     private init() {
         self.cancellables = .init()
-        
         bind()
     }
-    
+
     private func bind() {
         self.retrieve(.suggestions)
             .sink { completion in
@@ -94,13 +98,13 @@ final class ItemManager {
                 self.suggestions = workouts
             }
             .store(in: &cancellables)
-        
+
         self.retrieve(.workouts)
             .sink { completion in
                 switch completion {
                 case .finished:
                     print("finished")
-                    
+
                 case .failure(let error):
                     print(error.localizedDescription)
                 }
@@ -108,5 +112,70 @@ final class ItemManager {
                 self.itemToDos = workouts
             }
             .store(in: &cancellables)
+    }
+}
+
+
+final class WorkoutManager {
+    static let shared = WorkoutManager()
+    
+    private let defaults = UserDefaults.standard
+    
+    @Published private(set) var favorites: [WorkoutModel] = []
+    @Published private(set) var workoutList: [WorkoutModel] = []
+    
+    
+    
+    //------->
+    func saveWorkoutList(_ workout: WorkoutResponse) {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(workout.workouts)
+            defaults.setValue(encodedFavorites, forKey: workout.date.description)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func retrieveWorkoutList(of date: Date) -> AnyPublisher<[WorkoutModel], Error> {
+        guard let data = defaults.object(forKey: date.description) as? Data else {
+            return Just([]).setFailureType(to: Error.self).eraseToAnyPublisher()
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let workouts = try decoder.decode([WorkoutModel].self, from: data)
+            return Just(workouts).setFailureType(to: Error.self).eraseToAnyPublisher()
+        } catch {
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+    }
+    
+    func saveFavorite(of workouts: [WorkoutModel])   {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(workouts)
+            defaults.setValue(encodedFavorites, forKey: "favorites")
+        } catch {
+            print(error)
+        }
+    }
+    
+    func retrieveFavorites() -> AnyPublisher<[WorkoutModel], Error> {
+        guard let data = defaults.object(forKey: "favorites") as? Data else {
+            return Just([]).setFailureType(to: Error.self).eraseToAnyPublisher()
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let workouts = try decoder.decode([WorkoutModel].self, from: data)
+            return Just(workouts).setFailureType(to: Error.self).eraseToAnyPublisher()
+        } catch {
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+    }
+    
+    init() {
+        
     }
 }
