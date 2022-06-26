bbaf0f4a7533586ab1630d2655d7533dad6967fa
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-26 23:58:14 +0900

    feat: set nav for RootViewController

--- Adrenalist/Adrenalist/SceneDelegate.swift
+++ Adrenalist/Adrenalist/SceneDelegate.swift
@@ -18,7 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController:  MainViewController())
+        let vc = MainViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        nav.setNavigationBarHidden(true, animated: true)
+//        window?.rootViewController = nav
+        window?.rootViewController = vc
         window?.makeKeyAndVisible()
         
         window?.overrideUserInterfaceStyle = .dark
