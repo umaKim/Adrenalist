8a6d7ea8d21a75e3eeede598fa27f5377936f185
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-26 23:20:55 +0900

    feat: set up data stream for ContentViewController

--- Adrenalist/Adrenalist/Core/CalendarView/CalendarViewController.swift
+++ Adrenalist/Adrenalist/Core/CalendarView/CalendarViewController.swift
@@ -5,8 +5,13 @@
 //  Created by 김윤석 on 2022/06/19.
 //
 
+import Combine
 import FSCalendar
 import UIKit
+
+protocol ContentViewControllerDelegate: AnyObject {
+    func didSelectDate(_ date: Date)
+}
 
 final class ContentViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
     private lazy var grabberView: UIView = {
@@ -20,11 +25,54 @@ final class ContentViewController: UIViewController, FSCalendarDataSource, FSCal
     
     private let calendar = FSCalendar()
     
+    weak var delegate: ContentViewControllerDelegate?
+    
+    var selectedDate: Date = Date().stripTime()
+    
+    private let workoutManager = Manager.shared
+    
+    private var cancellables: Set<AnyCancellable>
+    
+    init() {
+        self.cancellables = .init()
+        super.init(nibName: nil, bundle: nil)
+    
+        workoutManager
+            .retrieve()
+            .sink { completion in
+            
+        } receiveValue: { responses in
+            self.workouts = responses
+            self.calendar.reloadData()
+        }
+        .store(in: &cancellables)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private var workouts: [WorkoutResponse] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupCalendar()
         setupUI()
+    }
+    
+    public func update(with date: Date) {
+        self.selectedDate = date.stripTime()
+        self.calendar.select(self.selectedDate)
+        self.calendar.reloadData()
+    }
+    
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        workouts.contains(where: {$0.date == date}) ? 1 : 0
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        delegate?.didSelectDate(date.stripTime())
     }
     
     private func setupCalendar() {
@@ -41,19 +89,19 @@ final class ContentViewController: UIViewController, FSCalendarDataSource, FSCal
         calendar.appearance.headerTitleColor = .white //2021년 1월(헤더) 색
 //        calendar.appearance.headerTitleFont = UIFont.systemFont(ofSize: 24) //타이틀 폰트 크기
                
-               
-            //MARK: -캘린더(날짜 부분) 관련
+        //MARK: -캘린더(날짜 부분) 관련
 //        calendar.backgroundColor = .white // 배경색
         calendar.appearance.weekdayTextColor = .white //요일(월,화,수..) 글씨 색
         calendar.appearance.selectionColor = .purpleBlue.withAlphaComponent(0.5) //선택 된 날의 동그라미 색
         calendar.appearance.titleWeekendColor = .white //주말 날짜 색
         calendar.appearance.titleDefaultColor = .white //기본 날짜 색
                 
-                
-                //MARK: -오늘 날짜(Today) 관련
+        //MARK: -오늘 날짜(Today) 관련
         calendar.appearance.titleTodayColor = .white //Today에 표시되는 특정 글자색
         calendar.appearance.todayColor = .purpleBlue //Today에 표시되는 선택 전 동그라미 색
 //        calendar.appearance.todaySelectionColor = .none
+        
+        calendar.select(self.selectedDate)
     }
     
     private func setupUI() {
