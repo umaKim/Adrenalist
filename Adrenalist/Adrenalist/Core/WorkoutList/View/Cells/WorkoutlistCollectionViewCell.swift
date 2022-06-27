7188461177cc37a13694042f3a5c715427318dd4
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-27 21:21:51 +0900

    feat: set up modeView and its data stream

--- Adrenalist/Adrenalist/Core/WorkoutList/View/Cells/WorkoutlistCollectionViewCell.swift
+++ Adrenalist/Adrenalist/Core/WorkoutList/View/Cells/WorkoutlistCollectionViewCell.swift
@@ -10,22 +10,7 @@ import UIKit
 final class WorkoutlistCollectionViewCell: UICollectionViewCell {
     static let identifier = "WorkoutlistCollectionViewCell"
     
-    private lazy var checkCircleButton: UIButton = {
-       let bt = UIButton()
-        bt.setImage(UIImage(systemName: "circle"), for: .normal)
-        bt.widthAnchor.constraint(equalToConstant: 24).isActive = true
-        bt.heightAnchor.constraint(equalToConstant: 24).isActive = true
-        bt.isHidden = true
-        return bt
-    }()
-    
-    private lazy var moveableMarkView: UIImageView = {
-       let uv = UIImageView(image: UIImage(systemName: "line.3.horizontal"))
-        uv.widthAnchor.constraint(equalToConstant: 20).isActive = true
-        uv.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        uv.isHidden = true
-        return uv
-    }()
+    private lazy var modeView = ModeView(mode: .normal)
     
     private lazy var titleLabel: UILabel = {
        let lb = UILabel()
@@ -59,7 +44,7 @@ final class WorkoutlistCollectionViewCell: UICollectionViewCell {
         self.cancellables = .init()
         super.init(frame: frame)
         
-//        setupUI()
+        setupUI()
         backgroundColor = .navyGray
     }
     
@@ -67,75 +52,15 @@ final class WorkoutlistCollectionViewCell: UICollectionViewCell {
     
     private var cancellables: Set<AnyCancellable>
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-//        switch model?.mode {
-//        case .normal:
-//            UIView.animate(withDuration: 1, delay: 0, options: .curveLinear) {
-//                self.checkCircleButton.isHidden = true
-//                self.moveableMarkView.isHidden = true
-//            } completion: { _ in }
-//
-//        case .reorder:
-//            UIView.animate(withDuration: 1, delay: 0, options: .showHideTransitionViews) {
-//                self.checkCircleButton.isHidden = true
-//                self.moveableMarkView.isHidden = false
-//            } completion: { _ in }
-//            
-//        case .psotpone:
-//            UIView.animate(withDuration: 1, delay: 0, options: .curveLinear) {
-//                self.checkCircleButton.isHidden = false
-//                self.moveableMarkView.isHidden = true
-//            } completion: { _ in }
-//            
-//        case .delete:
-//            UIView.animate(withDuration: 1, delay: 0, options: .curveLinear) {
-//                self.checkCircleButton.isHidden = false
-//                self.moveableMarkView.isHidden = true
-//            } completion: { _ in }
-//        case .none:
-//            break
-//        }
-    }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     private var model: WorkoutModel?
     
-    func configure(with model: WorkoutModel) {
+    func configure(with model: WorkoutModel, mode: WorkoutListCellMode) {
         self.model = model
-        
         self.isChecked = model.isSelected
-        
-//        switch model.mode {
-//        case .normal:
-//            UIView.animate(withDuration: 1, delay: 0, options: .curveLinear) {
-//                self.checkCircleButton.isHidden = true
-//                self.moveableMarkView.isHidden = true
-//            } completion: { _ in }
-//
-//        case .reorder:
-//            UIView.animate(withDuration: 1, delay: 0, options: .showHideTransitionViews) {
-//                self.checkCircleButton.isHidden = true
-//                self.moveableMarkView.isHidden = false
-//            } completion: { _ in }
-//
-//        case .psotpone:
-//            UIView.animate(withDuration: 1, delay: 0, options: .curveLinear) {
-//                self.checkCircleButton.isHidden = false
-//                self.moveableMarkView.isHidden = true
-//            } completion: { _ in }
-//
-//        case .delete:
-//            UIView.animate(withDuration: 1, delay: 0, options: .curveLinear) {
-//                self.checkCircleButton.isHidden = false
-//                self.moveableMarkView.isHidden = true
-//            } completion: { _ in }
-//        }
-        
         self.titleLabel.text = model.title
         
         if let reps = model.reps{
@@ -150,79 +75,43 @@ final class WorkoutlistCollectionViewCell: UICollectionViewCell {
             self.timerLabel.text = "\(timer) sec"
         }
         
-        checkCircleButton
-            .tapPublisher
-//            .compactMap({self.isChecked})
-            .sink { _ in
-                self.isChecked?.toggle()
-                let image = self.isChecked ?? false ? UIImage(systemName: "checkmark.circle.fill") : UIImage(systemName: "circle")
-                self.checkCircleButton.setImage(image, for: .normal)
+        self.modeView.updateMode(mode)
+        
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
+                self.modeView.isHidden = mode == .normal
+            } completion: { _ in }
+        }
+    }
+    
+    private func bind() {
+        self.modeView
+            .actionPublisher
+            .sink { action in
+                switch action {
+                case .tapCheckButton(let isTapped):
+                    break
+                }
             }
             .store(in: &cancellables)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        model = nil
+//        modeView = .init(mode: .delete)
         titleLabel.text = nil
         repsLabel.text = nil
         weightLabel.text = nil
         timerLabel.text = nil
     }
     
-//    private func anime() {
-//        let labelStackView = UIStackView(arrangedSubviews: [moveableMarkView, checkCircleButton, titleLabel])
-//        labelStackView.distribution = .fill
-//        labelStackView.alignment = .fill
-//        labelStackView.spacing = 6
-//        labelStackView.axis = .horizontal
-//        labelStackView.frame = .init(x: 16,
-//                                     y: 0,
-//                                     width: frame.width/2.2,
-//                                     height: frame.height)
-//
-//        contentView.addSubview(labelStackView)
-//    }
-//
-//    private func setupFrame() {
-//        layer.cornerRadius = 20
-//
-//        backgroundColor = .navyGray
-//
-//        let labelHStack = UIStackView(arrangedSubviews: [repsLabel, weightLabel])
-//        labelHStack.axis = .horizontal
-//        labelHStack.distribution = .fill
-//        labelHStack.alignment = .fill
-//
-//        let labelVStack = UIStackView(arrangedSubviews: [labelHStack, timerLabel])
-//        labelVStack.axis = .vertical
-//        labelVStack.distribution = .fill
-//        labelVStack.alignment = .fill
-//
-//        [
-//            checkCircleButton,
-//            moveableMarkView,
-//            titleLabel,
-//            labelVStack
-//        ].forEach { uv in
-//            contentView.addSubview(uv)
-//        }
-//        let cellFrameOrigin = frame.origin
-//
-//        labelHStack.frame = .init(x: 16,
-//                                  y: 20,
-//                                  width: frame.width / 2,
-//                                  height: frame.height)
-//        print(cellFrameOrigin)
-//        print(frame)
-//        print(frame.height)
-//    }
-    
     private func setupUI() {
         layer.cornerRadius = 20
         
         backgroundColor = .navyGray
         
-        let titleHStack = UIStackView(arrangedSubviews: [moveableMarkView, checkCircleButton, titleLabel])
+        let titleHStack = UIStackView(arrangedSubviews: [modeView, titleLabel])
         titleHStack.axis = .horizontal
         titleHStack.distribution = .fill
         titleHStack.alignment = .fill
@@ -249,12 +138,11 @@ final class WorkoutlistCollectionViewCell: UICollectionViewCell {
         
         NSLayoutConstraint.activate([
             titleHStack.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            titleHStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titleHStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             titleHStack.trailingAnchor.constraint(equalTo: contentView.centerXAnchor),
             
             labelVStack.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             labelVStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
         ])
     }
-    
 }
