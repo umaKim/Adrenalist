f4320683054d675f4ab9ab4905d16999c65aca70
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-29 21:37:04 +0900

    fix: fix editting problem by giving branch

--- Adrenalist/Adrenalist/Core/WorkoutSetup/WorkoutSetupViewModel.swift
+++ Adrenalist/Adrenalist/Core/WorkoutSetup/WorkoutSetupViewModel.swift
@@ -8,38 +8,52 @@
 import Combine
 import Foundation
 
-enum WorkoutSetupViewModelNotification{
-    case update(WorkoutModel)
+enum WorkoutSetupViewModelNotification {
+    
+}
+
+enum WorkoutSetupType: String {
+    case edit = "Edit"
+    case add = "Add"
 }
 
 class WorkoutSetupViewModel {
-    
-//    private(set) var workout = PassthroughSubject<WorkoutModel?, Never>()
     
     private(set) lazy var notifyPublisher = notifySubject.eraseToAnyPublisher()
     private let notifySubject = PassthroughSubject<WorkoutSetupViewModelNotification, Never>()
     
     private(set) var workout: WorkoutModel?
     
+    private(set) var type: WorkoutSetupType
+    
     private var title: String = ""
     private var isFavorite: Bool = false
     private var reps: Int = 0
     private var weight: Double = 0
     private var timer: TimeInterval = 0
-    private var set: Int = 1
+    private(set) var set: Int = 1
     
     private var cancellables: Set<AnyCancellable>
     
     init(
-        workout: WorkoutModel? = nil
+        workout: WorkoutModel? = nil,
+        type: WorkoutSetupType
     ) {
         self.workout = workout
+        self.type = type
         self.cancellables = .init()
         
-//        guard
-//            let workout = workout
-//        else { return }
-//        self.notifySubject.send(.update(workout))
+        // If given workout exist,
+        // set the variables with the given workout
+        // example case: didSelect cell for editing purpose
+        if let workout = workout {
+            title = workout.title
+            isFavorite = false
+            reps = workout.reps ?? 0
+            weight = workout.weight ?? 0
+            timer = workout.timer ?? 0
+            set = 1
+        }
     }
     
     func setTitle(_ text: String) {
@@ -66,39 +80,28 @@ class WorkoutSetupViewModel {
         self.isFavorite = isFavorite
     }
     
-    func doneDidTap() {
-        WorkoutManager.shared.retrieveWorkoutList(of: Date().stripTime())
-            .sink { comp in
-                
-            } receiveValue: { workouts in
-                var response = WorkoutResponse(date: Date().stripTime(),
-                                               workouts: workouts)
-                
-                let newWorkout = Array(repeating: WorkoutModel(uuid: UUID(),
-                                                               mode: .normal,
-                                                               title: self.title,
-                                                               reps: self.reps,
-                                                               weight: self.weight,
-                                                               timer: self.timer,
-                                                               isFavorite: self.isFavorite,
-                                                               isSelected: false,
-                                                               isDone: false),
-                                       count: self.set)
-                
-                response.workouts.append(contentsOf: newWorkout)
-                WorkoutManager.shared.saveWorkoutList(response)
-            }
-            .store(in: &cancellables)
-        
-        if isFavorite {
-            WorkoutManager.shared.retrieveFavorites().sink { comp in
-                
-            } receiveValue: { workouts in
-                var workouts = workouts
-                workouts.append(WorkoutModel(uuid: UUID(), mode: .normal, title: self.title, reps: self.reps, weight: self.weight, timer: self.timer, isFavorite: self.isFavorite, isSelected: false, isDone: false))
-                WorkoutManager.shared.saveFavorite(of: workouts)
-            }
-            .store(in: &cancellables)
+    func doneButtonDidTap() {
+        switch type {
+        case .edit:
+            guard let uuid = workout?.uuid else { return }
+            
+            self.workout = .init(uuid: uuid,
+                                 title: title,
+                                 reps: reps,
+                                 weight: weight,
+                                 timer: timer,
+                                 isFavorite: isFavorite,
+                                 isSelected: workout?.isSelected ?? false,
+                                 isDone: workout?.isDone ?? false)
+        case .add:
+            self.workout = .init(uuid: UUID(),
+                                 title: title,
+                                 reps: reps,
+                                 weight: weight,
+                                 timer: timer,
+                                 isFavorite: isFavorite,
+                                 isSelected: workout?.isSelected ?? false,
+                                 isDone: workout?.isDone ?? false)
         }
     }
 }
