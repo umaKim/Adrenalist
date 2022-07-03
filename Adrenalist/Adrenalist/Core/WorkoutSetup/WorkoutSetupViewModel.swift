b8cd15a42f9c434f42cbc799d0ac249ed0d79941
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-03 16:03:50 +0900

    chore: logic for appending multiple workouts

--- Adrenalist/Adrenalist/Core/WorkoutSetup/WorkoutSetupViewModel.swift
+++ Adrenalist/Adrenalist/Core/WorkoutSetup/WorkoutSetupViewModel.swift
@@ -9,7 +9,7 @@ import Combine
 import Foundation
 
 enum WorkoutSetupViewModelNotification {
-    
+    case doneButtonDidTap(models: [WorkoutModel], type: WorkoutSetupType)
 }
 
 enum WorkoutSetupType: String {
@@ -22,8 +22,7 @@ class WorkoutSetupViewModel {
     private(set) lazy var notifyPublisher = notifySubject.eraseToAnyPublisher()
     private let notifySubject = PassthroughSubject<WorkoutSetupViewModelNotification, Never>()
     
-    private(set) var workout: WorkoutModel?
-    
+    private(set) var workout: WorkoutModel
     private(set) var type: WorkoutSetupType
     
     private var title: String = ""
@@ -31,77 +30,64 @@ class WorkoutSetupViewModel {
     private var reps: Int = 0
     private var weight: Double = 0
     private var timer: TimeInterval = 0
-    private(set) var set: Int = 1
+    private var set: Int = 1
     
     private var cancellables: Set<AnyCancellable>
     
+    //MARK: - Init
     init(
-        workout: WorkoutModel? = nil,
+        workout: WorkoutModel,
         type: WorkoutSetupType
     ) {
         self.workout = workout
+        self.title = workout.title
+        self.reps = workout.reps ?? 0
+        self.weight = workout.weight ?? 0
+        self.timer = workout.timer ?? 0
         self.type = type
         self.cancellables = .init()
-        
-        // If given workout exist,
-        // set the variables with the given workout
-        // example case: didSelect cell for editing purpose
-        if let workout = workout {
-            title = workout.title
-            isFavorite = false
-            reps = workout.reps ?? 0
-            weight = workout.weight ?? 0
-            timer = workout.timer ?? 0
-            set = 1
-        }
     }
-    
-    func setTitle(_ text: String) {
+}
+
+//MARK: - Public Methods
+extension WorkoutSetupViewModel {
+    public func setTitle(_ text: String) {
         self.title = text
     }
     
-    func setReps(_ text: String) {
+    public func setReps(_ text: String) {
         self.reps = Int(text) ?? 0
     }
     
-    func setWeight(_ text: String) {
+    public func setWeight(_ text: String) {
         self.weight = Double(text) ?? 0
     }
     
-    func setTimer(_ text: String) {
+    public func setTimer(_ text: String) {
         self.timer = TimeInterval(text) ?? 0
     }
     
-    func setSet(_ text: String) {
+    public func setSet(_ text: String) {
         self.set = Int(text) ?? 0
     }
     
-    func setFavorite(_ isFavorite: Bool) {
+    public func setFavorite(_ isFavorite: Bool) {
         self.isFavorite = isFavorite
     }
     
-    func doneButtonDidTap() {
-        switch type {
-        case .edit:
-            guard let uuid = workout?.uuid else { return }
-            
-            self.workout = .init(uuid: uuid,
-                                 title: title,
-                                 reps: reps,
-                                 weight: weight,
-                                 timer: timer,
-                                 isFavorite: isFavorite,
-                                 isSelected: workout?.isSelected ?? false,
-                                 isDone: workout?.isDone ?? false)
-        case .add:
-            self.workout = .init(uuid: UUID(),
-                                 title: title,
-                                 reps: reps,
-                                 weight: weight,
-                                 timer: timer,
-                                 isFavorite: isFavorite,
-                                 isSelected: workout?.isSelected ?? false,
-                                 isDone: workout?.isDone ?? false)
+    public func doneButtonDidTap() {
+        var workouts: [WorkoutModel] = []
+        for _ in 1...set {
+            workouts.append(WorkoutModel(uuid: type == .add ? UUID() : workout.uuid,
+                                         title: title,
+                                         reps: reps,
+                                         weight: weight,
+                                         timer: timer,
+                                         isFavorite: isFavorite,
+                                         isSelected: workout.isSelected,
+                                         isDone: workout.isDone))
         }
+        
+        notifySubject.send(.doneButtonDidTap(models: workouts, type: type))
     }
 }
