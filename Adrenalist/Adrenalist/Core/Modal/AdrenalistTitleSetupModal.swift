a39c64b2dda20cb19058ca4c9a1975ebf1fb840e
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-07 22:02:13 +0900

    chore: refactor

--- Adrenalist/Adrenalist/Core/Modal/AdrenalistTitleSetupModal.swift
+++ Adrenalist/Adrenalist/Core/Modal/AdrenalistTitleSetupModal.swift
@@ -16,7 +16,7 @@ protocol ModalViewControllerDelegate: AnyObject {
 }
 
 final class ModalViewController: UIViewController {
-    private lazy var modal = AdrenalistTitleSetupModal()
+    private lazy var modal = AdrenalistTitleSetupModalView()
     
     weak var delegate: ModalViewControllerDelegate?
     
@@ -67,87 +67,3 @@ final class ModalViewController: UIViewController {
     }
 }
 
-
-enum AdrenalistTitleSetupModalAction {
-    case titleDidChange(String)
-    case confirmDidTap
-    case cancelDidTap
-}
-
-class AdrenalistTitleSetupModal: UIView {
-    
-    private(set) lazy var actionPublisher = actionSubject.eraseToAnyPublisher()
-    private var actionSubject = PassthroughSubject<AdrenalistTitleSetupModalAction, Never>()
-    
-    private lazy var setupTitle: UILabel = {
-       let lb = UILabel()
-        lb.text = "Set name"
-        return lb
-    }()
-    
-    private lazy var titleTextField = AdrenalistTextField(placeHolder: "Title")
-    
-    private lazy var confirmButton = AdrenalistTextRectangleButton(title: "confirm")
-    private lazy var cancelButton = AdrenalistTextRectangleButton(title: "cancel")
-    
-    private var cancellables: Set<AnyCancellable>
-    
-    init() {
-        self.cancellables = .init()
-        super.init(frame: .zero)
-        
-        bind()
-        setupUI()
-    }
-    
-    private func bind() {
-        titleTextField
-            .textPublisher
-            .receive(on: RunLoop.main)
-            .compactMap({$0})
-            .sink { text in
-                self.actionSubject.send(.titleDidChange(text))
-            }
-            .store(in: &cancellables)
-        
-        confirmButton.tapPublisher.sink { _ in
-            self.actionSubject.send(.confirmDidTap)
-        }
-        .store(in: &cancellables)
-        
-        cancelButton.tapPublisher.sink { _ in
-            self.actionSubject.send(.cancelDidTap)
-        }
-        .store(in: &cancellables)
-    }
-    
-    private func setupUI() {
-        backgroundColor = .blue
-        let sv = UIStackView(arrangedSubviews: [setupTitle, titleTextField])
-        sv.axis = .vertical
-        sv.alignment = .fill
-        sv.distribution = .fill
-        
-        let buttonSv = UIStackView(arrangedSubviews: [cancelButton, confirmButton])
-        buttonSv.axis = .horizontal
-        buttonSv.spacing = 8
-        buttonSv.alignment = .fill
-        buttonSv.distribution = .fillEqually
-        
-        [sv, buttonSv].forEach { uv in
-            uv.translatesAutoresizingMaskIntoConstraints = false
-            addSubview(uv)
-        }
-        
-        NSLayoutConstraint.activate([
-            sv.centerXAnchor.constraint(equalTo: centerXAnchor),
-            sv.centerYAnchor.constraint(equalTo: centerYAnchor),
-            buttonSv.topAnchor.constraint(equalTo: sv.bottomAnchor, constant: 8),
-            buttonSv.centerXAnchor.constraint(equalTo: centerXAnchor)
-        ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
