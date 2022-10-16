f78263097bdad7aebb3c88c1dc03f77ac5ec8355
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-16 15:13:28 +0900

    chore: bind

--- Adrenalist/Adrenalist/Core/FavoriteDetail/FavoriteSetList/FavoriteSetListViewModel.swift
+++ Adrenalist/Adrenalist/Core/FavoriteDetail/FavoriteSetList/FavoriteSetListViewModel.swift
@@ -6,19 +6,36 @@
 //
 
 import Combine
+import Foundation
 
 enum FavoriteSetListViewModelNotification {
-    
+    case reload
 }
 
 class FavoriteSetListViewModel {
     
-    private lazy var notifyPublisher = notifySubject.eraseToAnyPublisher()
+    private(set) lazy var notifyPublisher = notifySubject.eraseToAnyPublisher()
     private let notifySubject = PassthroughSubject<FavoriteSetListViewModelNotification, Never>()
     
     private(set) var response: WorkoutResponse
     
     init(with response: WorkoutResponse) {
         self.response = response
+        self.workoutList = response.workouts
+        
+        favoriteManager
+            .$favorites
+            .sink { responses in
+                let index = responses.firstIndex(where: {$0.uuid == response.uuid})!
+                self.response = responses[index]
+                self.workoutList = responses[index].workouts
+                self.notifySubject.send(.reload)
+            }
+            .store(in: &cancellables)
+    }
+    
+    private var cancellables: Set<AnyCancellable>
+    
+    private let favoriteManager = FavoriteSetManager.shared
     }
 }
